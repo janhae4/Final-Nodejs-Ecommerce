38e25387a0f03f1690611b88294b667076e2ebb2
janhae04 <hlchy11997@gmail.com>
2025-04-28 11:03:03 +0700

    feat(order): seperate controller from route

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,55 @@
+const orderService = require('../services/orderService');
+
+exports.getAllOrders = async (req, res) => {
+    try {
+        const orders = await orderService.getAllOrders();
+        res.status(200).json(orders);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.createOrder = async (req, res) => {
+    try {
+        const order = await orderService.createOrder(req.body);
+        res.status(201).json(order);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.patchOrder = async (req, res) => {
+    try {
+        const orderId = req.params.orderId;
+        const order = await orderService.patchOrder(orderId, req.body);
+        if (!order) {
+            return res.status(404).json({ message: 'Order not found' });
+        }
+        res.json(order);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.getOrderById = async (req, res) => {
+    try {
+        const orderId = req.params.orderId;
+        const order = await orderService.getOrderById(orderId);
+        if (!order) {
+            return res.status(404).json({ message: 'Order not found' });
+        }
+        res.json(order);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.getUserOrders = async (req, res) => {
+    try {
+        const user = req.user;
+        const orders = await orderService.getOrdersByUser(user._id);
+        res.json(orders);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
