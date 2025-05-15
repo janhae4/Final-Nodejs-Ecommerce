315ab5d7da1c4864ec694d721921a2f3d3ae0871
chhy <hlchy11997@gmail.com>
2025-05-15 19:08:47 +0700

    fix changepassword

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const authService = require('../services/authService');
+const authService = require("../services/authService");
 
 exports.register = async (req, res) => {
   try {
@@ -11,7 +11,18 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    const user = await authService.loginUser(req.body, res);
+    const user = await authService.loginUser(req.body);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    // Gửi token qua HttpOnly cookie
+    res.cookie("token", token, {
+      httpOnly: true, // Giới hạn truy cập từ JavaScript
+      secure: process.env.NODE_ENV === "production", // Chỉ sử dụng trên HTTPS khi production
+      sameSite: "strict", // Cookie chỉ gửi theo cùng domain
+      maxAge: 24 * 60 * 60 * 1000, // Thời gian sống cookie (24 giờ)
+    });
     res.json({ user });
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -25,7 +36,7 @@ exports.logout = (req, res) => {
     sameSite: "strict",
   });
 
-  res.json({ message: "Logged out successfully" })
+  res.json({ message: "Logged out successfully" });
 };
 
 exports.changePassword = async (req, res) => {
@@ -45,10 +56,14 @@ exports.changePassword = async (req, res) => {
 // Google callback route
 exports.googleCallback = async (req, res) => {
   try {
-    const { token, encodedUser } = await authService.handleGoogleCallback(req.user);
+    const { token, encodedUser } = await authService.handleGoogleCallback(
+      req.user
+    );
     res.cookie("token", token, { httpOnly: true });
     // ✅ Chuyển hướng về trang chủ kèm token và user
-    res.redirect(`http://localhost:5173/auth/oauth-success?user=${encodedUser}`);
+    res.redirect(
+      `http://localhost:5173/auth/oauth-success?user=${encodedUser}`
+    );
   } catch (error) {
     console.error("Error in googleCallback:", error);
     res.redirect("http://localhost:5173/auth/login?error=oauth_failed");
@@ -58,9 +73,13 @@ exports.googleCallback = async (req, res) => {
 // Facebook callback route
 exports.facebookCallback = async (req, res) => {
   try {
-    const { token, encodedUser } = await authService.handleFacebookCallback(req.user);
+    const { token, encodedUser } = await authService.handleFacebookCallback(
+      req.user
+    );
     res.cookie("token", token, { httpOnly: true });
-    res.redirect(`http://localhost:5173/auth/oauth-success?user=${encodedUser}`);
+    res.redirect(
+      `http://localhost:5173/auth/oauth-success?user=${encodedUser}`
+    );
   } catch (error) {
     console.error("Facebook login error:", error);
     res.status(500).json({ message: "Đăng nhập với Facebook thất bại" });
